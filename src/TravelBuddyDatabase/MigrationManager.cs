9966db2ad68549990291dc94f326470cd9ac1d58
stipek <stipek@cngroup.dk>
2017-03-08 08:34:48 +0100

    Přidána DB Factory

---
+++ src/TravelBuddyDatabase/MigrationManager.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelBuddyDatabase
+{
+    public class MigrationManager : IDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext Create(DbContextFactoryOptions options)
+        {
+            var connectionString = options.EnvironmentName == "Production"
+                ? "Server=tcp:a4amv.database.windows.net,1433;Initial Catalog=A4AMV;Persist Security Info=False;User ID=stipekp;Password=SuperTajneHeslo1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+                : "Server=(localdb)\\mssqllocaldb;Database=travelBuddy;Trusted_Connection=True;MultipleActiveResultSets=true;";
+
+            var factory = new ApplicationDbContextFactory(connectionString);
+            return factory.Create();
+        }
+    }
+}
