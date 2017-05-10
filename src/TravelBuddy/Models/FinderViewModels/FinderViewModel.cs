c496f8ad0d5e1814633dadeeb874a135cbca0998
a4amv <urbanj@cngroup.dk>
2017-05-10 17:08:30 +0200

    Finder add photos (#3)

--- src/TravelBuddy/Models/FinderViewModels/FinderViewModel.cs
+++ src/TravelBuddy/Models/FinderViewModels/FinderViewModel.cs
@@ -11,7 +11,7 @@ namespace TravelBuddy.Models.FinderViewModel
         public string Name { get; set; }
         public string city { get; set; }
         public string tags { get; set; }
-
+        public string PathToImage { get; set; }
 
     }
 }
